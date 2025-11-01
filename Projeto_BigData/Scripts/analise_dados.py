d5cbabda279e9f8a97ba9fcf96a8763a0f01b613
LuizCostaQxDigleT <lpcosta3011@gmail.com>
2025-11-01 07:52:38 -0300

    2

--- Projeto_BigData/Scripts/analise_dados.py
+++ Projeto_BigData/Scripts/analise_dados.py
@@ -1,6 +1,4 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-df = pd.read_excel("./lista2.xls", sheet_name='lista_2')
+df_xls_lista2 = pd.read_excel("/dados/lista2.xls", sheet_name="Lista 2")
