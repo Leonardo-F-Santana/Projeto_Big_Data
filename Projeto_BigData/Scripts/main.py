dc789060d18528ec06d9205166b4cdd79db76c19
viniciusvilla <120747413+viniciusvilla@users.noreply.github.com>
2025-11-14 20:12:22 -0300

    Modificações main.py / lista2.xlsx
    Ta rodando um gráfico bunitin.

--- Projeto_BigData/Scripts/main.py
+++ Projeto_BigData/Scripts/main.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
-df=pd.read_excel('./lista2.xls')
-print(df.head())
+# Pega o caminho absoluto da pasta onde o script está
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Monta o caminho para a planilha independente da máquina
+excel_path = os.path.join(BASE_DIR, "../Dados/lista2.xlsx")
+
+df = pd.read_excel(excel_path)
+
+df_numerico = df.select_dtypes(include=['float64', 'int64'])
+
+print(df_numerico.head())
 
 plt.figure(figsize=(10,8))
-sns.scatterplot(df.corr(), annot=True, cmap='coolwarm', square=True)
-plt.title('Gráfico de calor')
+sns.heatmap(df_numerico.corr(), annot=True, cmap='coolwarm', square=True)
+plt.title('Matriz de Correlação (Somente Variáveis Numéricas)')
 plt.show()
